8e9c2673b8c278ccc57a3a3e21aab56757563e7b
spavkov <spavkov@gmail.com>
2017-03-12 19:43:16 +0100

    Initial commit

---
+++ FakeApi/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FakeApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public async Task<IEnumerable<string>> Get()
+        {
+            //await Task.Delay(120000);
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public async Task<HttpResponseMessage> Post([FromBody]string value)
+        {
+            //await Task.Delay(120000);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TimeoutTestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeoutTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var client = new HttpClient() { Timeout = TimeSpan.FromSeconds(3) })
+                {
+                    var result =
+                        client.GetAsync(@"http://localhost:9999/api/values").Result;
+
+                    Console.WriteLine($"{result.StatusCode} {result.Content.ReadAsStringAsync().Result}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
